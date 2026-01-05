599b8e30a69fd2e286bf8582272d61a797495c55
Simeon Great <stargatecares@gmail.com>
2026-01-05 01:20:58 +0100

    commit all changes

--- app/api/paypal/route.js
+++ app/api/paypal/route.js
@@ -1,6 +1,6 @@
 // app/api/paypal/route.js
 import { NextResponse } from 'next/server';
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from "../../../../lib/supabaseClient";
 
 export async function POST(request) {
   try {
